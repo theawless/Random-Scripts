eb2c8527ee2b5e31b1b75c07bd776d1fa66d6765
theawless <abhiabhiabhiabhinav@gmail.com>
2015-12-07 12:24:58 +0530

    fixed a few bugs

--- anti_spammer_group2.js
+++ anti_spammer_group2.js
@@ -1,4 +1,4 @@
-﻿var url="https://mbasic.facebook.com/groups/1549537631989156?refid=27";
+﻿var url="https://mbasic.facebook.com/groups/1549537631989156";
 var grp_name="script_test";
 var accepted_likes=50;
 var times_to_repeat=100000;
@@ -14,7 +14,7 @@ while(count<times_to_repeat)
 	test +="URL GOTO="+url+"\n";
 	test +="SET !VAR1 "+ safe_count +"\n";
 	test +="SET !TIMEOUT_STEP 2 "+"\n";
-	test +="TAG POS={{!VAR1}} TYPE=H3 ATTR=TXT:*<SP>shared<SP>*video*group:<SP>IITG* "+"\n";
+	test +="TAG POS={{!VAR1}} TYPE=H3 ATTR=TXT:*<SP>shared<SP>*video* "+"\n";
 	test +="TAG POS=R2 TYPE=DIV ATTR=TXT:*Full<SP>Story<SP>·<SP>·<SP>More EXTRACT=TXT "+"\n";
 	var error=iimPlay(test);
 	
@@ -25,7 +25,7 @@ while(count<times_to_repeat)
 	{
 		//alert(info+"  likes:"+likes);	
 	}	
-	if(error=-921)
+	if(error==-921)
 	{
 		test="";
 		test ="CODE:";
@@ -38,28 +38,30 @@ while(count<times_to_repeat)
 		var new_url=iimGetExtract();
 		//alert(new_url);
 		url=new_url;
+		safe_count=1;
 		
-	}
-	
-	test="";
-	test ="CODE:";
-
-	if(likes>accepted_likes)
-	{
-		safe_count++;
 	}
 	else
 	{
-		test +="SET !VAR1 "+ safe_count +"\n";
-		test +="TAG POS={{!VAR1}} TYPE=H3 ATTR=TXT:*<SP>shared<SP>*video*group:<SP>IITG* "+"\n";
-		test +="TAG POS=R1 TYPE=A ATTR=TXT:Full<SP>Story "+"\n";
-		test +="TAG POS=1 TYPE=A ATTR=TXT:Delete "+"\n";
-		test +="TAG POS=1 TYPE=INPUT:SUBMIT FORM=ACTION:/a/delete.php?* ATTR=*"+"\n";
+		test="";
+		test ="CODE:";
+	
+		if(likes>accepted_likes)
+		{
+			safe_count++;
+		}
+		else
+		{
+			test +="SET !VAR1 "+ safe_count +"\n";
+			test +="TAG POS={{!VAR1}} TYPE=H3 ATTR=TXT:*<SP>shared<SP>*video*"+"\n";
+			test +="TAG POS=R1 TYPE=A ATTR=TXT:Full<SP>Story "+"\n";
+			test +="TAG POS=1 TYPE=A ATTR=TXT:Delete "+"\n";
+			test +="TAG POS=1 TYPE=INPUT:SUBMIT FORM=ACTION:/a/delete.php?* ATTR=*"+"\n";
+		}
+		
+		iimPlay(test);
+		count++;
 	}
-	
-	iimPlay(test);
-	count++;
-	
 }
 
 function extract_like_num(str)
